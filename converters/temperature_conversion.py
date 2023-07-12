6ada6f8d483395b5c864c4da2370c9135d8bef20
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-12 22:52:16 +0100

    completed

--- converters/temperature_conversion.py
+++ converters/temperature_conversion.py
@@ -10,25 +10,66 @@ class TemperatureConversion(object):
 
     def convert(self):
         if self.amt1 == "invalid_amt1":
-            return "invalid_amt1"
+            return self.amt1
 
         if self.unit1 == "Celsius(°C)":
+            if self.amt1 < -273.15:
+                return "invalid_amt1"
             return self.convert_celsius()
+
         elif self.unit1 == "Fahrenheit(°F)":
-            return self.convert_farenheit()
+            if self.amt1 < -459.67:
+                return "invalid_amt1"
+            return self.convert_fahrenheit()
+
         elif self.unit1 == "Kelvin(K)":
+            if self.amt1 < 0:
+                return "invalid_amt1"
             return self.convert_kelvin()
+
         elif self.unit1 == "Rankine(°R)":
+            if self.amt1 < 0:
+                return "invalid_amt1"
             return self.convert_rankine()
 
         return "invalid_unit1"
 
     def convert_celsius(self):
         if self.unit2 == "Celsius(°C)":
-            return self.amt1
+            return self.amt1 * 1
         elif self.unit2 == "Fahrenheit(°F)":
-            return self.amt1
+            return self.amt1 * 1.8 + 32
         elif self.unit2 == "Kelvin(K)":
-            return self.amt1
+            return self.amt1 + 273.15
         elif self.unit2 == "Rankine(°R)":
-            return self.amt1
+            return (self.amt1 + 273.15) * 1.8
+
+    def convert_fahrenheit(self):
+        if self.unit2 == "Celsius(°C)":
+            return (self.amt1 - 32) * (5 / 9)
+        elif self.unit2 == "Fahrenheit(°F)":
+            return self.amt1 * 1
+        elif self.unit2 == "Kelvin(K)":
+            return (self.amt1 - 32) * (5 / 9) + 273.15
+        elif self.unit2 == "Rankine(°R)":
+            return self.amt1 + 459.67
+
+    def convert_kelvin(self):
+        if self.unit2 == "Celsius(°C)":
+            return self.amt1 - 273.15
+        elif self.unit2 == "Fahrenheit(°F)":
+            return (self.amt1 - 273.15) * 1.8 + 32
+        elif self.unit2 == "Kelvin(K)":
+            return self.amt1 * 1
+        elif self.unit2 == "Rankine(°R)":
+            return self.amt1 * 1.8
+
+    def convert_rankine(self):
+        if self.unit2 == "Celsius(°C)":
+            return (self.amt1 - 491.67) * (5 / 9)
+        elif self.unit2 == "Fahrenheit(°F)":
+            return self.amt1 - 459.67
+        elif self.unit2 == "Kelvin(K)":
+            return self.amt1 * (5 / 9)
+        elif self.unit2 == "Rankine(°R)":
+            return self.amt1 * 1
