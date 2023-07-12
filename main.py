7ca52a27cd3b35bc9dd7e037bdaeb19fd0c302c8
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-12 20:55:32 +0100

    conversions area finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from button import Button
+from area_conversion import AreaConversion
 from length_conversion import LengthConversion
 
 pygame.init()
@@ -136,9 +137,9 @@ def conversions_menu():
 
         Y_SPACING = HEIGHT * 0.1
 
-        TEXT = get_font(HEIGHT * 0.1).render("Conversions", True, HEX_BLUE)
-        RECT = TEXT.get_rect(center=(WIDTH * 0.5, Y_SPACING))
-        SCREEN.blit(TEXT, RECT)
+        text = get_font(HEIGHT * 0.1).render("Conversions", True, HEX_BLUE)
+        rect = text.get_rect(center=(WIDTH * 0.5, Y_SPACING))
+        SCREEN.blit(text, rect)
 
         AREA_BUTTON = Button(
             image=None,
@@ -240,7 +241,314 @@ def conversions_menu():
 
 
 def conversions_area_menu():
-    pass
+    unit1 = "square kilometer(km²)"
+    unit2 = "square meter(m²)"
+    amt1 = "1"
+    c = AreaConversion(amt1, unit1, unit2)
+    amt2 = str(c.convert())
+
+    while True:
+        MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill(HEX_GREY)
+
+        X_SPACING = WIDTH * 0.25
+        Y_SPACING = HEIGHT * 0.1
+
+        # Area
+        text = get_font(HEIGHT * 0.1).render("Area", True, HEX_BLUE)
+        rect = text.get_rect(center=(WIDTH * 0.5, Y_SPACING))
+        SCREEN.blit(text, rect)
+
+        # amt1
+        text = get_font(HEIGHT * 0.04).render(amt1, True, HEX_WHITE)
+        rect = text.get_rect(center=(WIDTH // 2 - X_SPACING, Y_SPACING * 3))
+        SCREEN.blit(text, rect)
+
+        # =
+        text = get_font(HEIGHT * 0.04).render("=", True, HEX_WHITE)
+        rect = text.get_rect(center=(WIDTH * 0.5, Y_SPACING * 4))
+        SCREEN.blit(text, rect)
+
+        # amt2
+        text = get_font(HEIGHT * 0.04).render(amt2, True, HEX_WHITE)
+        rect = text.get_rect(center=(WIDTH // 2 - X_SPACING, Y_SPACING * 5))
+        SCREEN.blit(text, rect)
+
+        UNIT1_BUTTON = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * 3),
+            text_input=unit1,
+            font=get_font(HEIGHT * 0.04),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+
+        UNIT2_BUTTON = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * 5),
+            text_input=unit2,
+            font=get_font(HEIGHT * 0.04),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+
+        BACK_BUTTON = Button(
+            image=None,
+            pos=(WIDTH * 0.4, HEIGHT - Y_SPACING),
+            text_input="Back",
+            font=get_font(HEIGHT * 0.05),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+
+        EXIT_BUTTON = Button(
+            image=None,
+            pos=(WIDTH * 0.6, HEIGHT - Y_SPACING),
+            text_input="Exit",
+            font=get_font(HEIGHT * 0.05),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+
+        for button in [
+            UNIT1_BUTTON,
+            UNIT2_BUTTON,
+            BACK_BUTTON,
+            EXIT_BUTTON,
+        ]:
+            button.changeColor(MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_0:
+                    amt1 += "0"
+                elif event.key == pygame.K_1:
+                    amt1 += "1"
+                elif event.key == pygame.K_2:
+                    amt1 += "2"
+                elif event.key == pygame.K_3:
+                    amt1 += "3"
+                elif event.key == pygame.K_4:
+                    amt1 += "4"
+                elif event.key == pygame.K_5:
+                    amt1 += "5"
+                elif event.key == pygame.K_6:
+                    amt1 += "6"
+                elif event.key == pygame.K_7:
+                    amt1 += "7"
+                elif event.key == pygame.K_8:
+                    amt1 += "8"
+                elif event.key == pygame.K_9:
+                    amt1 += "9"
+                elif event.key == pygame.K_PERIOD or event.key == pygame.K_COMMA:
+                    amt1 += "."
+                elif event.key == pygame.K_PLUS and (len(amt1) == 0 or amt1[-1] == "e"):
+                    amt1 += "+"
+                elif event.key == pygame.K_MINUS and (
+                    len(amt1) == 0 or amt1[-1] == "e"
+                ):
+                    amt1 += "-"
+                elif event.key == pygame.K_e and len(amt1) != 0:
+                    amt1 += "e"
+                elif event.key == pygame.K_ESCAPE:
+                    amt1 = ""
+                elif event.key == pygame.K_BACKSPACE:
+                    amt1 = amt1[:-1]
+
+                c = AreaConversion(amt1, unit1, unit2)
+                amt2 = str(c.convert())
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if UNIT1_BUTTON.checkForInput(MOUSE_POS):
+                    unit1 = conversions_area_menu_unit_selection(unit1, 0)
+                if UNIT2_BUTTON.checkForInput(MOUSE_POS):
+                    unit2 = conversions_area_menu_unit_selection(unit2, 2)
+                if BACK_BUTTON.checkForInput(MOUSE_POS):
+                    conversions_menu()
+                if EXIT_BUTTON.checkForInput(MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+                c = AreaConversion(amt1, unit1, unit2)
+                amt2 = str(c.convert())
+
+        pygame.display.update()
+
+
+def conversions_area_menu_unit_selection(curr_unit, flag):
+    while True:
+        MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill(HEX_GREY)
+
+        X_SPACING = WIDTH * 0.25
+        Y_SPACING = HEIGHT * 0.1
+        y_increment = 3.5
+
+        METER = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square meter(m²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        KILOMETER = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square kilometer(km²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        CENTIMETER = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square centimeter(cm²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        MILIMETER = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square milimeter(mm²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        MICROMETER = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square micrometer(μm²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        HECTARE = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="hectare(ha)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        ACRE = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="acre(ac)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        MILE = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square mile(mi²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        YARD = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square yard(yd²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        FOOT = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square foot(ft²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        INCH = Button(
+            image=None,
+            pos=(WIDTH - X_SPACING, Y_SPACING * (y_increment + flag)),
+            text_input="square inch(in²)",
+            font=get_font(HEIGHT * 0.025),
+            base_color=HEX_WHITE,
+            hovering_color=HEX_BLUE,
+        )
+        y_increment += 0.35
+
+        for button in [
+            METER,
+            KILOMETER,
+            CENTIMETER,
+            MILIMETER,
+            MICROMETER,
+            HECTARE,
+            ACRE,
+            MILE,
+            YARD,
+            FOOT,
+            INCH,
+        ]:
+            button.changeColor(MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return curr_unit
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if METER.checkForInput(MOUSE_POS):
+                    return "square meter(m²)"
+                if KILOMETER.checkForInput(MOUSE_POS):
+                    return "square kilometer(km²)"
+                if CENTIMETER.checkForInput(MOUSE_POS):
+                    return "square centimeter(cm²)"
+                if MILIMETER.checkForInput(MOUSE_POS):
+                    return "square milimeter(mm²)"
+                if MICROMETER.checkForInput(MOUSE_POS):
+                    return "square micrometer(μm²)"
+                if HECTARE.checkForInput(MOUSE_POS):
+                    return "hectare(ha)"
+                if ACRE.checkForInput(MOUSE_POS):
+                    return "acre(ac)"
+                if MILE.checkForInput(MOUSE_POS):
+                    return "square mile(mi²)"
+                if YARD.checkForInput(MOUSE_POS):
+                    return "square yard(yd²)"
+                if FOOT.checkForInput(MOUSE_POS):
+                    return "square foot(ft²)"
+                if INCH.checkForInput(MOUSE_POS):
+                    return "square inch(in²)"
+
+        pygame.display.update()
 
 
 def conversions_length_menu():
