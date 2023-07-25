0885f17442ed699ac0d5a76c0ce83510a26d86d5
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-07-25 21:37:56 +0100

    finished DataConversion class

--- converters/data_conversion.py
+++ converters/data_conversion.py
@@ -9,49 +9,48 @@ class DataConversion(object):
         self.unit2 = unit2
 
     def convert(self):
+        bit_amt = "invalid_bit_amt"
+
         if self.amt1 == "invalid_amt1":
             return self.amt1
+        elif self.unit1 == "bit(b)":
+            bit_amt = self.amt1 * 1
+        elif self.unit1 == "Byte(B)":
+            bit_amt = self.amt1 * 8
+        elif self.unit1 == "word":
+            bit_amt = self.amt1 * 16
+        elif self.unit1 == "block":
+            bit_amt = self.amt1 * 4096
+        elif self.unit1 == "kiloByte(kB)":
+            bit_amt = self.amt1 * 8192
+        elif self.unit1 == "megaByte(MB)":
+            bit_amt = self.amt1 * 8388608
+        elif self.unit1 == "gigaByte(GB)":
+            bit_amt = self.amt1 * 8589934592
+        elif self.unit1 == "teraByte(TB)":
+            bit_amt = self.amt1 * 8796093022208
+        elif self.unit1 == "petaByte(PB)":
+            bit_amt = self.amt1 * 9007199254740640
 
-        if self.unit1 == "bit(b)":
-            return self.convert_bit()
+        return self.convert_bit(bit_amt)
 
-        if self.unit1 == "Byte(B)":
-            return self.convert_byte()
-
-        if self.unit1 == "word":
-            return self.convert_word()
-
-        if self.unit1 == "block":
-            return self.convert_block()
-
-        if self.unit1 == "kiloByte(kB)":
-            return self.convert_kilobyte()
-
-        if self.unit1 == "megaByte(MB)":
-            return self.convert_megabyte()
-
-        if self.unit1 == "gigaByte(GB)":
-            return self.convert_gigabyte()
-
-        if self.unit1 == "teraByte(TB)":
-            return self.convert_terabyte()
-
-        if self.unit1 == "petaByte(PB)":
-            return self.convert_petabyte()
-
-        return "invalid_unit1"
-
-    def convert_bit(self):
-        if self.unit2 == "":
-            return self.amt1 * 1
-        if self.unit2 == "radian(π)":
-            return self.amt1 * 0.00555555555
-        if self.unit2 == "minute(')":
-            return self.amt1 * 60
-        if self.unit2 == "second('')":
-            return self.amt1 * 3600
-        if self.unit2 == "circle":
-            return self.amt1 * 0.0027777778
-        if self.unit2 == "quadrant":
-            return self.amt1 * 0.0111111110
+    def convert_bit(self, bit_amt):
+        if self.unit2 == "bit(b)":
+            return bit_amt * 1
+        if self.unit2 == "Byte(B)":
+            return bit_amt * 0.125
+        if self.unit2 == "word":
+            return bit_amt * 0.0625
+        if self.unit2 == "block":
+            return bit_amt * 0.0002441406
+        if self.unit2 == "kiloByte(kB)":
+            return bit_amt * 0.0001220703
+        if self.unit2 == "megaByte(MB)":
+            return bit_amt * 1.192092895e-7
+        if self.unit2 == "gigaByte(GB)":
+            return bit_amt * 1.164153218e-10
+        if self.unit2 == "teraByte(TB)":
+            return bit_amt * 1.136868377e-13
+        if self.unit2 == "petaByte(PB)":
+            return bit_amt * 1.110223024e-16
         return "invalid_unit2"
